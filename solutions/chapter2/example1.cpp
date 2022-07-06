cfc02663134e7a21d08ddd105383a7cf5c5f8425
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-07-06 16:28:46 +0500

    Further improved main() to use any limit N

--- solutions/chapter2/example1.cpp
+++ solutions/chapter2/example1.cpp
@@ -2,16 +2,18 @@
 
 #include <iostream>
 
+#define N 100
+
 int main()
 {
     std::string expansion;
     std::size_t partialSum = 0;
-    for(std::size_t i = 1; i < 10; ++i)
+    for(std::size_t i = 1; i < N; ++i)
     {
         expansion += std::to_string(i) + "^3 + ";
         std::cout << expansion << "\b\b:" << std::endl;
         partialSum += i * i * i;
-        std::cout << partialSum << std::endl;
+        std::cout << partialSum << std::endl << std::endl;
     }
     return 0;
 }
