19cd63d7ce393af95e39a4c4883b694c695386f1
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-07-07 02:13:52 +0500

    Improved algorithm from O(N2) to O(NlogN)

--- solutions/chapter2/max_sub_sum.cpp
+++ solutions/chapter2/max_sub_sum.cpp
@@ -1,6 +1,16 @@
 #include <vector>
 #include <iostream>
 
+#include "header.h"
+
+int max3(int n1, int n2, int n3)
+{
+    if(n1 >= n2 && n1 >= n3) return n1;
+    if(n2 >= n1 && n2 >= n3) return n2;
+    if(n3 >= n1 && n3 >= n2) return n3;
+    return 0; // <--- Should never happen
+}
+
 int maxSubSum1(const std::vector<int> &v)
 {
     int maxSum = 0;
@@ -34,12 +44,40 @@ int maxSubSum2(const std::vector<int> &v)
 
 int maxSubSum3(const std::vector<int> &v)
 {
-    return 0;
+    return maxSumRec(v, 0, v.size() - 1);
 }
 
 int maxSubSum4(const std::vector<int> &v)
 {
     return 0;
+}
+
+int maxSumRec(const std::vector<int> &v, std::vector<int>::size_type left, std::vector<int>::size_type right)
+{
+    if(left == right)
+        if(v[left] > 0)
+            return a[left];
+        return 0;
+    std::vector<int>::size_type center = (left + right) / 2;
+    int maxLeftSum = maxSumRec(v, left, center);
+    int maxRightSum = maxSumRec(v, center + 1, right);
+    int maxLeftBorderSum = 0;
+    int leftBorderSum = 0;
+    for(std::vector<int>::size_t idx = center; idx >= left; --idx)
+    {
+        leftBorderSum += v[idx];
+        if(leftBorderSum > maxLeftBorderSum)
+            maxLeftBorderSum = leftBorderSum;
+    }
+    int maxRightBorderSum = 0;
+    int rightBorderSum = 0;
+    for(std::vector<int>::size_type idx = center + 1; idx <= right; ++idx)
+    {
+        rightBorderSum += v[idx];
+        if(rightBorderSum > maxRightBorderSum)
+            maxRightBorderSum = rightBorderSum;
+    }
+    return max3(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
 }
 
 int main()
