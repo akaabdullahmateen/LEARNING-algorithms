7549623f3eb02feeea1fc1c816520754e809cbbb
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-07-07 20:47:18 +0500

    Created first draft

---
+++ solutions/chapter2/exponential.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define MAX_N 10
+
+typedef long long int64;
+
+int64 pow(int64 base, std::size_t n)
+{
+    if(!n)
+        return 1;
+    if(n % 2)
+        return pow(base, n / 2) * pow(base, n / 2) * base;
+    return pow(base, n / 2) * pow(base, n / 2);
+}
+
+int main()
+{
+    int64 base = 2;
+    for(std::size_t n = 0; n < MAX_N; ++n)
+        std::cout << base << "^" << n << "\t= " << pow(base, n) << std::endl;
+    return 0;
+}
