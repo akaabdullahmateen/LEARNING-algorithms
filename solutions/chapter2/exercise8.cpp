d9af30bb1f71fc85372f14a19c82a29037564be0
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-07-08 00:45:31 +0500

    Added runtime analysis with averages

--- solutions/chapter2/exercise8.cpp
+++ solutions/chapter2/exercise8.cpp
@@ -1,9 +1,10 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <string>
 #include <iostream>
-
-#define N 5
 
 std::size_t randInt(std::size_t min, std::size_t max)
 {
@@ -35,7 +36,7 @@ void swap(Container &a, typename Container::size_type i, typename Container::siz
     a[j] = temp;
 }
 
-void algo1()
+void algo1(std::size_t N)
 {
     std::vector<std::size_t> a;
     for(std::vector<std::size_t>::size_type i = 0; i < N; ++i)
@@ -46,10 +47,9 @@ void algo1()
         while(contains(a, random));
         a.push_back(random);
     }    
-    print(a);
 }
 
-void algo2()
+void algo2(std::size_t N)
 {
     std::vector<std::size_t> a;
     std::size_t used[N] = {0};
@@ -66,24 +66,46 @@ void algo2()
         }
         a.push_back(random);
     }    
-    print(a);
 }
 
-void algo3()
+void algo3(std::size_t N)
 {
     std::vector<std::size_t> a;
     for(std::vector<std::size_t>::size_type i = 0; i < N; ++i)
         a.push_back(i + 1);
     for(std::vector<std::size_t>::size_type i = 0; i < N; ++i)
         swap(a, i, randInt(0, i));
-    print(a);
+}
+
+void stopwatch(std::string name, void (*algoN)(std::size_t), std::size_t n)
+{
+    double runtimeSum = 0;
+    for(std::size_t count = 0; count < 10; ++count)
+    {
+        std::chrono::time_point start = std::chrono::steady_clock::now();
+        algoN(n);
+        std::chrono::time_point end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> runtime = end - start;    
+        runtimeSum += runtime.count();
+    }
+    double runtimeAvg = runtimeSum / 10;
+    std::cout << name << " ( " << n << " ) \t: " << std::fixed << std::setprecision(9) << std::left << std::setw(9) << runtimeAvg << std::endl;
 }
 
 int main()
 {
     std::srand(std::time(NULL));
-    algo1();
-    algo2();
-    algo3();
+
+    const std::size_t iter1[] = {250, 500, 1000, 2000};
+    const std::size_t iter2[] = {25000, 50000, 100000, 200000, 400000, 800000};
+    const std::size_t iter3[] = {100000, 200000, 400000, 800000, 1600000, 3200000, 6400000};
+
+    for(const std::size_t n : iter1)
+        stopwatch("[ALGO1]", algo1, n);
+    for(const std::size_t n :iter2)
+        stopwatch("[ALGO2]", algo2, n);
+    for(const std::size_t n : iter3)
+        stopwatch("[ALGO3]", algo3, n);
+
     return 0;
 }
