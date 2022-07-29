f78e3a2980d52e22ca0a3fe6bc790f1f16d5e355
Yahya Mateen <akaabdullahmateen@gmail.com>
2022-07-29 23:43:40 +0500

    Implement stack on top of vector class

--- stl-implementation/stack.h
+++ stl-implementation/stack.h
@@ -1,70 +1,19 @@
 #ifndef STACK_H
 #define STACK_H
 
-#define GUARANTEED_CAPACITY 2
+#include <vector>
 
 template<typename T>
-class sequence
-{
-public:
-    typedef const T&        const_reference;
-    typedef T&              reference;
-    typedef T*              iterator;
-    typedef T               value_type;
-    typedef std::size_type  size_type;
-
-    sequence() : m_size(0U), m_base(GUARANTEED_CAPACITY)
-    {
-        m_base = new T[GUARANTEED_CAPACITY];
-    }
-
-    template<typename... _Args>
-    void emplace(_Args &&...__args);
-    bool empty()
-    {
-        return m_size == 0;
-    }
-    void pop_back();
-    void push_back(const value_type &__x);
-    void push_back(value_type &&__x);
-    size_type size() const
-    {
-        return m_size;
-    }
-    reference back();
-    const_reference back() const;
-
-private:
-    iterator m_base;
-    size_type m_size;
-    size_type m_capacity;
-
-    void reallocate(size_type __capacity)
-    {
-        if(m_capacity >= __capacity) return;
-        iterator __tmp_base = new value_type[__capacity];
-        for(size_type idx = 0; idx != m_size; ++idx)
-            __tmp_base[idx] = m_base[idx];
-        std::destroy(m_base, m_base + m_size);
-        m_base = __tmp_base;
-        delete __tmp_base;
-    }
-};
-
 class stack
 {
-    typedef sequence::const_reference   const_reference;
-    typedef sequence::reference         reference;
-    typedef sequence::value_type        value_type;
-    typedef sequence::size_type         size_type;
+public:
+    typedef typename std::vector<T>::const_reference   const_reference;
+    typedef typename std::vector<T>::reference         reference;
+    typedef typename std::vector<T>::value_type        value_type;
+    typedef typename std::vector<T>::size_type         size_type;
 
-    stack(const sequence &__c);
-    stack(sequence &&__c = sequence());
-    template<typename... _Args>
-    void emplace(_Args &&...__args)
-    {
-        c.emplace(__args);
-    }
+    stack(const std::vector<T> &__c) : c{__c} {}
+    stack(std::vector<T> &&__c = std::vector<T>()) : c{__c} {};
     bool empty() const
     {
         return c.empty();
@@ -85,7 +34,6 @@ class stack
     {
         return c.size();
     }
-    void swap(stack &__s);
     reference top()
     {
         return c.back();
@@ -95,8 +43,8 @@ class stack
         return c.back();
     }
 
-    private:
-        sequence c;
+private:
+        std::vector<T> c;
 };
 
 #endif
