e7d9eebd21febb5082c493b02587dd30a3ed9c96
Yahya Mateen <akaabdullahmateen@gmail.com>
2022-07-31 20:22:50 +0500

    Reformatted indentation

--- solutions/chapter3/p6.c
+++ solutions/chapter3/p6.c
@@ -5,50 +5,51 @@ typedef struct Node* List;
 typedef struct Node* Position;
 struct Node
 {
-int Number;
-Position Next;
+    int Number;
+    Position Next;
 };
 void Insert( int X , Position P )
 {
-Position TmpCell;
-TmpCell = (struct Node*) malloc(sizeof(struct Node));
-TmpCell->Number = X;
-TmpCell->Next = P->Next;
-P->Next = TmpCell;
+    Position TmpCell;
+    TmpCell = (struct Node*) malloc(sizeof(struct Node));
+    TmpCell->Number = X;
+    TmpCell->Next = P->Next;
+    P->Next = TmpCell;
 }
 void DeleteNext( Position P )
 {
-Position TmpCell;
-TmpCell = P->Next;
-P->Next = TmpCell->Next;
-free(TmpCell);
+    Position TmpCell;
+    TmpCell = P->Next;
+    P->Next = TmpCell->Next;
+    free(TmpCell);
 }
 int Josephus( int N , int M )
 {
-List L;
-Position LastCell;
-int i, count=0;
-// Create a circularly linked list with N nodes
-L = (struct Node*) malloc(sizeof(struct Node));
-L->Number = 1;
-LastCell = L->Next = L;
-for ( i = 2 ; i <= N ; i++ ) {
-Insert( i , LastCell );
-LastCell = LastCell->Next;
+    List L;
+    Position LastCell;
+    int i, count=0;
+    // Create a circularly linked list with N nodes
+    L = (struct Node*) malloc(sizeof(struct Node));
+    L->Number = 1;
+    LastCell = L->Next = L;
+    for ( i = 2 ; i <= N ; i++ ) {
+        Insert( i , LastCell );
+        LastCell = LastCell->Next;
+    }
+    // Perform the elimination process
+    while ( L != L->Next ) {
+        if ( ++count == M ) {
+            DeleteNext(L);
+            count = 0;
+        }
+        L = L->Next;
+    }
+    return L->Number;
 }
-// Perform the elimination process
-while ( L != L->Next ) {
-if ( ++count == M ) {
-DeleteNext(L);
-count = 0;
-}
-L = L->Next;
-}
-return L->Number;
-}
-void main()
+int main()
 {
-int N,M;
-scanf("%d %d" , &N , &M );
-printf("The winner is %d\n" , Josephus( N , M ));
+    int N,M;
+    scanf("%d %d" , &N , &M );
+    printf("The winner is %d\n" , Josephus( N , M ));
+    return 0;
 }
